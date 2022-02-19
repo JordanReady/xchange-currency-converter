58890a1a326d7d20e438c1b005276e6a7361c630
JordanReady <jready112@gmail.com>
2022-02-19 17:25:02 -0600

    basic react components

---
+++ src/CurrencyConverter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CurrencyConverter() {
+  return (
+    <div>
+        CurrencyConverter
+    </div>
+  )
+}
---
+++ src/CurrencyRates.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CurrencyRates() {
+  return (
+    <div>CurrencyRates</div>
+  )
+}

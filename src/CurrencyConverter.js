01d019be802ce35a387856e73957a7777e8f120c
JordanReady <jready112@gmail.com>
2022-02-20 22:32:43 -0600

    built currency converter and simple footer

--- src/CurrencyConverter.js
+++ src/CurrencyConverter.js
@@ -2,8 +2,43 @@ import React from 'react'
 
 export default function CurrencyConverter() {
   return (
-    <div>
-        CurrencyConverter
+    <div className='container wrapper'>
+        <h1>Currency Converter</h1>
+        <form action='#'>
+            <div className='amount'>
+                <p>Enter Amount</p>
+                <input type={'text'}></input>
+            </div>
+            <div className='drop-list'>
+                <div className='from'>
+                    <p>From</p>
+                    <div className='select-box'>
+                        <select>
+                            <option value={'USD'}>USD</option>
+                            <option value={'NPR'}>NPR</option>
+                            <option value={'INR'}>INR</option>
+                            <option value={'PKR'}>PKR</option>
+                            <option value={'MAD'}>MAD</option>
+                        </select>
+                    </div>
+                </div>
+                <div className='icon'>.</div>
+                <div className='from'>
+                    <p>To</p>
+                    <div className='select-box'>
+                        <select>
+                            <option value={'NPR'}>NPR</option>
+                            <option value={'USD'}>USD</option>
+                            <option value={'INR'}>INR</option>
+                            <option value={'PKR'}>PKR</option>
+                            <option value={'MAD'}>MAD</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <button className='btn'>Find Exhange Rate</button>
+            <div className='exchange-rate'>X currency = Y currency</div>
+        </form>
     </div>
   )
 }
--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <h5>If you want to see more, check out my socials!</h5>
+      <div>
+        <div className='m-2 social'> <a className='social-link' href='https://github.com/JordanReady/xchange-currency-converter' target={'_blank'}>GitHub</a></div>
+        <div className='m-2 social'> <a className='social-link' href='https://hungry-euler-0a8a98.netlify.app/' target={'_blank'}>Portfolio</a></div>
+      </div>
+    </div>
   )
 }
 

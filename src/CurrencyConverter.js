d781e436a4cda311a201656203bf9158c8ca050b
JordanReady <jready112@gmail.com>
2022-02-28 18:47:21 -0600

    adding chart for historical rates

--- src/CurrencyConverter.js
+++ src/CurrencyConverter.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, } from 'react'
+import Chart from 'chart.js/auto';
 
 export default function CurrencyConverter(props) {
 
@@ -41,6 +42,10 @@ export default function CurrencyConverter(props) {
         const conversion = (fromCurrencyAmount * exchangeRate).toFixed(2);
         
         setToCurrencyAmount(conversion);
+        console.log(chartLabels);
+        console.log(chartLabel);
+        console.log(chartRate);
+        console.log(chartData);
     }
 
     const getAmount = (e) => {
@@ -55,6 +60,75 @@ export default function CurrencyConverter(props) {
     const getToCurrency = (e) => {
         setToCurrency(e.target.value);
     }
+
+
+    // Chart.js
+    const [chartLabels, setChartLabels] = useState([]);
+    const [chartRate, setChartRate] = useState([]);
+    const [chartLabel, setChartLabel] = useState([]);
+    const [chartData, setChartData] = useState([]);
+    const endDate = new Date().toISOString().split('T')[0];
+    const startDate = new Date((new Date).getTime() - (30 * 24 * 60 * 60 * 1000)).toISOString().split('T')[0];
+
+
+    const chart = () => {
+        if(fromCurrency & toCurrency !== undefined) {
+            fetch(`https://altexchangerateapi.herokuapp.com/${startDate}..${endDate}?from=${fromCurrency}&to=${toCurrency}`)
+            .then(res => res.json())
+            .then(data => {
+                setChartLabels([...Object.keys(data.rates)]);
+                setChartRate([...Object.values(data.rates).map(rate => rate[toCurrency])]);
+                setChartLabel(`${fromCurrency}/${toCurrency}`);
+                setChartData({
+                    type: 'line',
+                    data: {
+                        labels: chartLabels,
+                        datasets: [{
+                            label: chartLabel,
+                            data: chartRate,
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                })
+            })
+            .catch(err => {
+                console.log(err);
+              });
+        }
+    }
+
+    useEffect(() => {
+        chart();
+        const ctx = document.getElementById('myChart').getContext('2d');
+        const myChart = new Chart(ctx, chartData);
+        if (typeof myChart !== 'undefined') {
+            myChart.destroy();
+        }
+    }, [fromCurrency, toCurrency]);
+
 
   return (
     <div className='container wrapper my-5'>
@@ -90,6 +164,7 @@ export default function CurrencyConverter(props) {
             <button className='btn find-button' onClick={handleClick}>Find Exhange Rate</button>
             <div className='exchange-rate'> {fromCurrencyAmount} {fromCurrency} = {toCurrencyAmount} {toCurrency}</div>
         </form>
+        <canvas id='myChart' width={'310'} height={'310'} />
     </div>
   )
 }
